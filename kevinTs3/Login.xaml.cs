35f85f3c1dbedaca8e45424f577bc37a759adc4a
master <master@SMLAB408PC11_23>
2022-12-13 19:36:15 -0500

    Agregar archivos de proyecto.

---
+++ kevinTs3/Login.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace kevinTs3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "kevinT";
+            string contraseña = "1234";
+            if(usuario== txtUsuario.Text && contraseña== txtContraseña.Text)
+            {
+                Navigation.PushAsync(new Registro(usuario));
+            }
+            else
+            {
+                DisplayAlert("ALERTA", "Usuario/Contraseña Incorrectos", "Cerrar");
+            }
+        }
+    }
+}
---
+++ kevinTs3/Registro.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace kevinTs3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro(string usuario)
+        {
+            InitializeComponent();
+            lblUsuario.Text = usuario;
+
+        }
+    }
+}
